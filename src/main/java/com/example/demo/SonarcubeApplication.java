05a20ab2fd562d054cccbcb00db75a7d9aa168e4
maharsuresh <54435775+maharsuresh@users.noreply.github.com>
2020-01-29 01:48:45 +0530

    Update SonarcubeApplication.java

--- src/main/java/com/example/demo/SonarcubeApplication.java
+++ src/main/java/com/example/demo/SonarcubeApplication.java
@@ -8,7 +8,7 @@ public class SonarcubeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonarcubeApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello world");
 	}
 
 }
