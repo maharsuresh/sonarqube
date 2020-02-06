08443d1e2e126608ad0c946080ccef2e03aaa332
maharsuresh <54435775+maharsuresh@users.noreply.github.com>
2020-02-06 19:50:06 +0530

    Update sonarqube-analysis.sh

--- sonarqube-analysis.sh
+++ sonarqube-analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Source environment
-source ~/.bashrc
+#source ~/.bashrc
 
 export SONAR_VERSION="4.1.0.1829"
 
@@ -13,7 +13,7 @@ fi
 
 
 SONAR_HOME=`find "$PWD" -type d -name 'sonar-scanner*'`
-if [[ ! "$SONAR_HOME" =~ sonar-scanner ]]; then
+if [ ! "$SONAR_HOME" =~ sonar-scanner ]; then
     echo "Failed to setup sonar binaries"
     exit
 else
